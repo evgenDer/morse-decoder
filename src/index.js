7b4f7d40466338459bf08f43910e4e70511b31b3
Derevygo Evgeniya <evgeniya.nice@yandex.by>
2020-03-17 23:51:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,12 +35,38 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let result='';
+    let temp;
+    let array=expr.match(/[0-9*]{1,10}/g);
+    let newArray=array.map(elem=>elem.match(/[0-9*]{1,2}/g));
+    for(let i=0;i<newArray.length;i++){
+      for(let j=0;j<5;j++){
+          if(newArray[i][j]=='10'){
+            newArray[i][j]='.';}
+          else if(newArray[i][j]=='11'){
+             newArray[i][j]='-';
+          }
+          else if(newArray[i][j]=='00')
+             newArray[i][j]='';
+        }
+        temp=newArray[i].join("").replace(/[*]{1,10}/,' ');
+        console.log(temp);
+        result+=MORSE_TABLE[temp];
+      }
+          
+    console.log(newArray);
+    console.log(array);
+    console.log(result);
+    return result;
 
+}
+console.log(decode('0011101010***************'));
 module.exports = {
     decode
 }
+
+console.log(decode('0011101010***************'));
